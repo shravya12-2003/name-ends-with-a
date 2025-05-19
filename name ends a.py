42db4a132c9f6d92c4d73201e7085a6e97d8ede4
shravya12-2003 <sravs200312@gmail.com>
2025-05-19 09:57:46 +0530

    Add files via upload

---
+++ name ends a.py
@@ -0,0 +1,9 @@
+name = input('Name: ')
+
+# Check if name ends with 'a'
+if name == 'ends with a':
+    print('Name ends with "a"')
+elif not name.endswith('a'):
+    print("Name doesn't end with 'a'")
+else:
+    print('Name ends with "a"')
